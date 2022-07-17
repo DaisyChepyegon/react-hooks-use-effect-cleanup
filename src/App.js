ea04794ba07620858547b469e21a7da8a0292e18
DaisyChepyegon <daisychepyegon@gmail.com>
2022-07-17 14:02:15 +0300

    Done.

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
   return (
     <div>
       {showClock ? <Clock /> : null}
-      <button onClick={() => setShowClock(!showClock)}>Toggle Clock</button>
+      <button 
+      onClick={() => setShowClock(!showClock)}>Toggle Clock
+      </button>
     </div>
   );
 }
